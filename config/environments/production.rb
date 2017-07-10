426c67698c7d7e08dd031296001453e6c48a1f1f
Elijah Omolo <eomolo11@gmail.com>
2017-07-09 23:09:37 -0500

    Modified production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,5 +86,5 @@ Rails.application.configure do
 end
 
 # Required for Devise. Remember to change localhost:3000 to actual application host
-config.action_mailer.default_url_options = { host: 'localhost:3000' }
-end
+#config.action_mailer.default_url_options = { host: 'localhost:3000' }
+# end
